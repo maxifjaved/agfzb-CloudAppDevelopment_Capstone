41afb639ca5d4db4cff7a15b5b74d8cc71cb72b2
Muhammad Asif Javed <axif.javed@gmail.com>
2023-07-09 14:39:01 +0500

    Hands-on Labs: Create Django Proxy Services Of Cloud Functions

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarModel
+from .restapis import get_dealers_from_cf, get_dealer_by_id_from_cf, get_dealer_reviews_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -93,12 +93,64 @@ def registration_request(request):
 def get_dealerships(request):
   context = {}
   if request.method == "GET":
+    url = "https://jp-tok.functions.appdomain.cloud/api/v1/web/8767a390-c1a2-4465-adee-608645d9cc2b/dealership-package/get-dealership"
+    # Get dealers from the URL
+    dealerships = get_dealers_from_cf(url)
+    # Concat all dealer's short name
+    dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+    context['dealer_names'] = dealer_names
+    # Return a list of dealer short name
     return render(request, 'djangoapp/index.html', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+  if request.method == "GET":
+    context = {}
+    dealer_url = "https://jp-tok.functions.appdomain.cloud/api/v1/web/8767a390-c1a2-4465-adee-608645d9cc2b/dealership-package/get-dealership"
+    dealer = get_dealer_by_id_from_cf(dealer_url, id=dealer_id)
+    context["dealer"] = dealer
+    review_url = "https://jp-tok.functions.appdomain.cloud/api/v1/web/8767a390-c1a2-4465-adee-608645d9cc2b/dealership-package/get-review"
+    reviews = get_dealer_reviews_from_cf(review_url, id=dealer_id)
+    context["reviews"] = reviews
+
+    return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+  context = {}
+  dealer_url = "https://jp-tok.functions.appdomain.cloud/api/v1/web/2e1cc6b1-4125-4ddf-983a-23203b79e5e7/dealership-package/get-dealership"
+  dealer = get_dealer_by_id_from_cf(dealer_url, id=dealer_id)
+  context["dealer"] = dealer
+  if request.method == 'GET':
+    # Get cars for the dealer
+    cars = CarModel.objects.all()
+    print(cars)
+    context["cars"] = cars
+
+    return render(request, 'djangoapp/add_review.html', context)
+  elif request.method == 'POST':
+    if request.user.is_authenticated:
+      username = request.user.username
+      print(request.POST)
+      payload = dict()
+      car_id = request.POST["car"]
+      car = CarModel.objects.get(pk=car_id)
+      payload["time"] = datetime.utcnow().isoformat()
+      payload["name"] = username
+      payload["dealership"] = dealer_id
+      payload["id"] = dealer_id
+      payload["review"] = request.POST["content"]
+      payload["purchase"] = False
+      if "purchasecheck" in request.POST:
+        if request.POST["purchasecheck"] == 'on':
+          payload["purchase"] = True
+          payload["purchase_date"] = request.POST["purchasedate"]
+          payload["car_make"] = car.make.name
+          payload["car_model"] = car.name
+          payload["car_year"] = int(car.year)
+
+      new_payload = {}
+      new_payload["review"] = payload
+      review_post_url = "https://jp-tok.functions.appdomain.cloud/api/v1/web/8767a390-c1a2-4465-adee-608645d9cc2b/dealership-package/post-review"
+      post_request(review_post_url, new_payload, id=dealer_id)
+    return redirect("djangoapp:dealer_details", id=dealer_id)
